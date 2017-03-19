7a67a0f8b80121a32365a9fdc26276866b180506
Mishaux <mishaux@gmail.com>
2017-03-18 19:20:14 -0600

    Add pix_fmt option to encoding_options

--- lib/ffmpeg/encoding_options.rb
+++ lib/ffmpeg/encoding_options.rb
@@ -149,6 +149,10 @@ module FFMPEG
       ["-profile:v", value]
     end
 
+#    def convert_pix_fmt(value)
+#      ["-pix_fmt", value]
+#    end
+
     def convert_watermark_filter(value)
       case value[:position].to_s
       when "LT"
